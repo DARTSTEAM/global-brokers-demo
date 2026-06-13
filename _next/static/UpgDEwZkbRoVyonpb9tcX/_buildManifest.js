54518ad74ab27a0db91d57cc848a3dd1cc0646a5
Juan Pablo Brea <juanbrearuiz@gmail.com>
2026-06-14 00:21:35 +0200

    v3 - UX overhaul

---
+++ _next/static/UpgDEwZkbRoVyonpb9tcX/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/global-brokers-demo//_next/:path+",
+        "destination": "/global-brokers-demo/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
